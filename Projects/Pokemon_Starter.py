a39e97ddd369b3c7494b905e48da7db51f87d324
ketmo23 <ketmo23@users.noreply.github.com>
2022-06-09 15:21:41 +0000

    project

---
+++ Projects/Pokemon_Starter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+round = 0
+while True:
+    round = round + 1
+#Intro
+print(input("Hello! Welcome to the world of Pokemon!"))
+
+answer = input("Are you ready to start your journey? \n")
+
+if answer == "yes":
+    print("Great! Before we begin.. \n")
+    
+
+elif answer == "no":
+    print("Do you wish to quit?")
+    
+
+else:
+    print("Try again!")
+
+name = input("What is your name? \n")
+
+
+print(name,"lets determine your starter pokemon!")
+
+
+
+
