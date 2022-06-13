fb69c1a83be692b42f024dfe82a167b7720e86e3
ketmo23 <ketmo23@users.noreply.github.com>
2022-06-13 14:55:59 +0000

    jun13

--- Projects/Pokemon_Starter.py
+++ Projects/Pokemon_Starter.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
-
 #Intro
-print(input("Hello! Welcome to the world of Pokemon!"))
+print("Hello! Welcome to the world of Pokemon! ")
+
+input()
 
 answer = input("Are you ready to start your journey? \n")
 
-if answer == "yes":
+if answer.lower() == "yes":
     print("Great! Before we begin.. \n")
-    
+    name = input("What is your name? \n")
+    print(name,"lets determine your starter pokemon! ")
 
-elif answer == "no":
-    print("Do you wish to quit?")
-    
+elif answer.lower() == "no":
+    answer = input("Do you wish to quit? \n")
+    if answer.lower() == "yes":
+        print("But I thought you wanted to be the best there ever was? ")
+        answer = input("Are you sure? \n")
+        if answer.lower() == "yes":
+             print("But you must catch them all!! ")
+             input()
+             name = input("What is your name young pokemon master? \n")
 
-else:
-    print("Try again!")
+input()
+year = int(input("choose a year between 1996-2004 \n"))
 
-name = input("What is your name? \n")
-
-
-print(name,"lets determine your starter pokemon!")
-
-
-
-
+#while year 
+#gen1 
+#gen2
+#gen3
