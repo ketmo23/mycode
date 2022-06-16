ec6216723333e0644cc58a0729a1b27a79b2f48b
ketmo23 <ketmo23@users.noreply.github.com>
2022-06-16 17:58:52 +0000

    jun16

--- Projects/Pokemon_Starter.py
+++ Projects/Pokemon_Starter.py
@@ -1,31 +1,58 @@
 #!/usr/bin/env python3
 
-#Intro
+#total affinity for pokemon type 
+affinity_total = 0
+
+#Introduction name and location of character 
 print("Hello! Welcome to the world of Pokemon! ")
 
 input()
 
-answer = input("Are you ready to start your journey? \n")
+answer = input("Are you ready to start your journey? \n>")
 
 if answer.lower() == "yes":
     print("Great! Before we begin.. \n")
-    name = input("What is your name? \n")
-    print(name,"lets determine your starter pokemon! ")
+    name = input("What is your name? \n>")
 
 elif answer.lower() == "no":
-    answer = input("Do you wish to quit? \n")
+    answer = input("Do you wish to quit? \n>")
     if answer.lower() == "yes":
         print("But I thought you wanted to be the best there ever was? ")
-        answer = input("Are you sure? \n")
+        answer = input("Are you sure? \n>")
         if answer.lower() == "yes":
              print("But you must catch them all!! ")
-             input()
+            
              name = input("What is your name young pokemon master? \n")
 
 input()
-year = int(input("choose a year between 1996-2004 \n"))
+location = input("Where are you from? \n>")
+input()
+print(name,"lets determine your starter pokemon! ") 
 
-#while year 
-#gen1 
-#gen2
-#gen3
+while true:
+    q1_ans = input("Do you wish to have wings and fly away? \na) Yes away we go!, \b) No thanks i'll walk, \nc) You dont need wings to fly. \n>"
+
+            if q1_ans.lower() == "a":
+                
+                affinity_total = + 3
+                break
+            
+            elif q1_ans.lower() == "b":
+                
+                affinity_total = + 1
+                break
+            
+            elif q1_ans.lower() == "c":
+                
+                affinity_total = + 10
+                break
+            else :
+                input()
+                print("Please select an answer.")
+                input()
+                continue
+
+
+
+
+
